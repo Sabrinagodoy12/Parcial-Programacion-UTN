31ce991383027420ffe6ccefcb23ffdcb9026f8c
Sabrina Godoy <sabrygodoy1@gmail.com>
2025-09-24 14:18:30 -0300

    ejercicio 1 y 2

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Parcial-Programacion-UTN
 
+En este repositorio se encuentra el parcial que debemos desarrollar para la materia Programación 1 de la Tecnicatura en programación de la Universidad Tecnológica Nacional
+
+# Enunciado:
+
+La biblioteca escolar necesita un sistema de gestión sencillo para su catálogo de libros y las
+copias disponibles. Se pide desarrollar un programa con una interfaz basada en menú que
+utilice listas paralelas (una para titulos[] y otra para ejemplares[]). Cada título debe estar
+vinculado a su número correspondiente de copias utilizando el mismo índice en ambas listas.
+Se debe utilizar un bucle while para navegar por las opciones del menú hasta que el usuario
+elija salir.
+
+## Requerimientos del Menú
+
+1. Ingresar títulos → permite agregar los títulos de los libros.
+2. Ingresar ejemplares → cantidad de copias para cada título.
+3. Mostrar catálogo → muestra todos los libros con su stock.
+4. Consultar disponibilidad → busca un título y muestra cuántos ejemplares hay.
+5. Listar agotados → muestra los títulos con 0 ejemplares.
+6. Agregar título → permite sumar un nuevo libro al catálogo.
+7. Actualizar ejemplares (préstamo/devolución) → modifica el stock.
+8. Salir → termina el programa.
+
+## Requerimientos de estructuras
+
+Usar: While, estructura Case para el menú, estructuras condicionales, estructuras secuenciales en Python y realizar las validaciones solicitadas.
---
+++ parcial.py
@@ -0,0 +1,63 @@
+titulos = []
+ejemplares = []
+
+opcion = 0
+
+while opcion != 8:
+    opcion= int(input("\n--- Ingrese el número de la acción que desea realizar --- \n1. Ingresar títulos \n2. Ingresar ejemplares \n3. Mostrar el catálogo de libros \n4. Consultar disponibilidad \n5. Lista de libros agorados \n6. Agregar un nuevo libro \n7. Actualizar ejemplares \n8. Salir del menú \n"))
+    
+    match opcion:
+        #1. Ingresar títulos.
+        case 1:
+            titulo = input("Ingresar el título: ")
+            #Si el título ya se encuentra en títulos o el usuario no ingresó nada, le aparece un mensaje indicandole que vuelva a escribirlo.
+            
+            while titulo in titulos or titulo == "":
+                titulo = input("El título esta repetido o en blanco, por favor ingrese el título nuevamente: ")
+            
+            #Muestra el título ingresado y lo agrega a la lista.
+            print(f"El título ingresado es: {titulo}")
+            titulos.append(titulo)
+            posicion = titulos.index(titulo)
+            ejemplares.insert(posicion, 0)
+        
+        #2. Ingresar ejemplares
+        case 2:
+            if not titulos:
+                print("No existe ningún título en el catálogo. Primero ingrese un título en la opción 1 del menú si desea ingresar cantidad de ejemplares.")
+                continue
+
+            for i, titulo in enumerate(titulos):
+                print(f"{i + 1}- {titulo}")
+            
+            #Solicita la entrada y valida que sea correcta
+            while True:
+                entrada = input("\nSeleccione el número de título para ingresar ejemplares: ")
+
+                #Validación: valida que el usuario ingrese al menos algo, evitando que solo presione "Enter".
+                if not entrada.isdigit():  
+                    print("\nDebe ingresar un número válido.")
+                    continue
+                
+                posicion = int(entrada) - 1
+
+                #Validación: que se encuentre dentro del rango de nuestra lista
+                if 0 <= posicion < len(titulos):   
+                    break
+                else:
+                    print("\nPosición inválida. Seleccione el número del título correctamente.")
+
+            #Pedir cantidad de ejemplares con validación
+            while True:
+                entrada = input("\nIngrese la cantidad de ejemplares que desea agregar: ")
+                if not entrada.isdigit():
+                    print("Debe ingresar un número válido.")
+                    continue
+                cantidad = int(entrada)
+                break
+
+            #Actualización de ejemplares
+            ejemplares[posicion] += cantidad
+            print(f"\nEjemplares disponibles para: {titulos[posicion]} --> {ejemplares[posicion]}")
+
+                
