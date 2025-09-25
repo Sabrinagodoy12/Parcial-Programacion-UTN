8f291ad452f14ff6b6a72f07907277010f0b8137
Sabrina Godoy <sabrygodoy1@gmail.com>
2025-09-25 16:40:59 -0300

    Corrección ejercicio 2, agregado de ejercicios 3 y 4

--- parcial.py
+++ parcial.py
@@ -4,18 +4,18 @@ ejemplares = []
 opcion = 0
 
 while opcion != 8:
-    opcion= int(input("\n--- Ingrese el número de la acción que desea realizar --- \n1. Ingresar títulos \n2. Ingresar ejemplares \n3. Mostrar el catálogo de libros \n4. Consultar disponibilidad \n5. Lista de libros agorados \n6. Agregar un nuevo libro \n7. Actualizar ejemplares \n8. Salir del menú \n"))
+    opcion= int(input("\n--- Ingrese el número de la acción que desea realizar --- \n1. Ingresar títulos \n2. Ingresar ejemplares \n3. Mostrar el catálogo de libros \n4. Consultar disponibilidad \n5. Lista de libros agotados \n6. Agregar un nuevo libro \n7. Actualizar ejemplares \n8. Salir del menú \n"))
     
     match opcion:
-        #1. Ingresar títulos.
+        #1. Ingresar títulos
         case 1:
             titulo = input("Ingresar el título: ")
-            #Si el título ya se encuentra en títulos o el usuario no ingresó nada, le aparece un mensaje indicandole que vuelva a escribirlo.
-            
+
+            #Si el título ya se encuentra en títulos o el usuario no ingresó nada, le aparece un mensaje indicandole que vuelva a escribirlo
             while titulo in titulos or titulo == "":
                 titulo = input("El título esta repetido o en blanco, por favor ingrese el título nuevamente: ")
             
-            #Muestra el título ingresado y lo agrega a la lista.
+            #Muestra el título ingresado y lo agrega a la lista
             print(f"El título ingresado es: {titulo}")
             titulos.append(titulo)
             posicion = titulos.index(titulo)
@@ -23,6 +23,7 @@ while opcion != 8:
         
         #2. Ingresar ejemplares
         case 2:
+            #Si no existe ningún título, le sale un mensaje al usuario
             if not titulos:
                 print("No existe ningún título en el catálogo. Primero ingrese un título en la opción 1 del menú si desea ingresar cantidad de ejemplares.")
                 continue
@@ -30,48 +31,72 @@ while opcion != 8:
             for i, titulo in enumerate(titulos):
                 print(f"{i + 1}- {titulo}")
             
-            #El usuario selecciona una posición y validamos si es correcta
-            posicion = int(input("Seleccione el número de título para ingresar ejemplares: ")) - 1
+            while True:
+                entrada = input("\nSeleccione el número de título para ingresar ejemplares: ")
 
-            while posicion < 0 or posicion >= len(titulos):
-              posicion = int(input("Posición inválida. Seleccione el número del título correctamente para ingresar ejemplares: ")) - 1 
+                #Validación: valida que el usuario ingrese al menos algo, evitando que solo presione "Enter"
+                if not entrada.isdigit():  
+                    print("\nDebe ingresar un número válido.")
+                    continue
+                
+                posicion = int(entrada) - 1
+
+                #Validación: que se encuentre dentro del rango de nuestra lista
+                if 0 <= posicion < len(titulos):   
+                    break
+                else:
+                    print("\nPosición inválida. Seleccione el número del título correctamente.")
+
+            #Pedir cantidad de ejemplares con validación
+            while True:
+                entrada = input("\nIngrese la cantidad de ejemplares que desea agregar: ")
+                if not entrada.isdigit():
+                    print("Debe ingresar un número válido.")
+                    continue
+                cantidad = int(entrada)
+                break
+
+            #Actualización de ejemplares
+            ejemplares[posicion] += cantidad
+            print(f"\nEjemplares disponibles para: {titulos[posicion]} --> {ejemplares[posicion]}")
+        
+        #3. Mostrar el catálogo de libros
+        case 3:
+            if not titulos:
+                print("No existe ningún título en el catálogo. Primero ingrese un título en la opción 1 del menú si desea ver nuestro catálogo.")
+                continue
+
+            print("\n-- Nuestro catálogo --")
+
+            for i in range(len(titulos)):
+                print(f"Libro: {titulos[i]} - Stock: {ejemplares[i]}")
+        
+        #4. Consultar disponibilidad
+        case 4:
+            if not titulos:
+                print("No existe ningún título en el catálogo. Primero ingrese un título en la opción 1 del menú si desea consultar nuestra disponibildad")
+                continue
             
-            cantidad = int(input("Ingrese la cantidad de ejemplares que desea agregar: ")) 
-            ejemplares[posicion] += cantidad 
-            print(f"Se han añadido: {ejemplares[posicion]} ejemplares a {titulos[posicion]} ")
+            busqueda_titulo = input("Ingrese el título que desea consultar: ").lower()
+
+            #Entra en el bucle hasta que la persona decida volver al menú
+            while busqueda_titulo != "salir":
+
+                #Si existe el título muestra el mensaje con los ejemplares que tiene y vuelve al menú
+                if busqueda_titulo in titulos:
+                    posicion = titulos.index(busqueda_titulo)
+                    print(f"Hay {ejemplares[posicion]} ejemplares disponibles, para el título: {busqueda_titulo}")
+                    break
+
+                #Si no existe el título muestra un mensaje y vuelve a habilitar la búsqueda
+                else:
+                    print(f"El título: {busqueda_titulo} no se encuentra en nuestro catálogo.")
+                    busqueda_titulo = input("Vuelva a ingresar un nombre válido. Si desea volver al menú escriba: salir. ")
+        
+        #5. Lista de libros agotados
+        case 5:
+            continue
 
 
 
 
-
-            # #Solicita la entrada y valida que sea correcta
-            # while True:
-            #     entrada = input("\nSeleccione el número de título para ingresar ejemplares: ")
-
-            #     #Validación: valida que el usuario ingrese al menos algo, evitando que solo presione "Enter".
-            #     if not entrada.isdigit():  
-            #         print("\nDebe ingresar un número válido.")
-            #         continue
-                
-            #     posicion = int(entrada) - 1
-
-            #     #Validación: que se encuentre dentro del rango de nuestra lista
-            #     if 0 <= posicion < len(titulos):   
-            #         break
-            #     else:
-            #         print("\nPosición inválida. Seleccione el número del título correctamente.")
-
-            # #Pedir cantidad de ejemplares con validación
-            # while True:
-            #     entrada = input("\nIngrese la cantidad de ejemplares que desea agregar: ")
-            #     if not entrada.isdigit():
-            #         print("Debe ingresar un número válido.")
-            #         continue
-            #     cantidad = int(entrada)
-            #     break
-
-            # #Actualización de ejemplares
-            # ejemplares[posicion] += cantidad
-            # print(f"\nEjemplares disponibles para: {titulos[posicion]} --> {ejemplares[posicion]}")
-
-                
